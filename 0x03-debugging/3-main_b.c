9f60bf7fdac3cfd01b5ba9cc8eff416aba2e147e
mamudou1 <mamudoubojang17@gmail.com>
2022-03-17 08:17:07 +0000

    again

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - takes a date and prints how many days are left in the year, taking
- * leap years into account
- * Return: 0
- */
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
 
 int main(void)
 {
@@ -13,9 +13,9 @@ int main(void)
 	int day;
 	int year;
 
-	month = 2;
-	day = 29;
-	year = 2000;
+	month = 10;
+	day = 4;
+	year = 1997;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
