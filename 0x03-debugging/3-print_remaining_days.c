8a1fc5fcba0e8a8bc29dd8d86a3e42a41c31803f
mamudou1 <mamudoubojang17@gmail.com>
2022-03-18 07:59:18 +0000

    last one

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,19 +3,18 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * print_remaining_days - func
+ * @month: variable 1
+ * @day: variable 2
+ * @year: variable 3
+ * Return: val
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
-		if (month >= 3 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
