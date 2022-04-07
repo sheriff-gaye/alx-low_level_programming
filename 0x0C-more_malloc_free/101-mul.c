19a44279e60e3a62587a0f2c4b889d5f100bef62
mamudou1 <mamudoubojang17@gmail.com>
2022-04-07 12:24:46 +0000

    t

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,71 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
-int main(int argc, char *argv[])
-{
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+ * *_realloc -is function that reallocates a memory block
+ * @ptr: is a pointer to the memory previously alloated with call
+ * @old_size: is the size in bytes of the allocated space for ptr
+ * @new_size: is the new size in bytes of the new memory block
+ *
+ * Return: Return free.
+**/
 
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+unsigned int i;
+char *array;
+
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+array = malloc(new_size);
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < new_size; i++)
+*(array + (i * new_size)) = 0;
+return (array);
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_realloc -is function that reallocates a memory block
+ * @ptr: is a pointer to the memory previously alloated with call
+ * @old_size: is the size in bytes of the allocated space for ptr
+ * @new_size: is the new size in bytes of the new memory block
+ *
+ * Return: Return free.
+**/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+{
+unsigned int i;
+char *array;
+
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+array = malloc(new_size);
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < new_size; i++)
+*(array + (i * new_size)) = 0;
+return (array);
 }
