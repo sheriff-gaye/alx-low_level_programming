f5dfcb64227b06d1d796aee15c63539e7146ec06
sheriff-gaye <sheriffgaye5@gmail.com>
2022-05-05 16:35:10 +0000

    char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+char c[] = "_putchar";
+int n = 0;
 
-	return (0);
+while (n <= 7)
+{
+  _putchar(c[n]);
+  n++;
 }
-
+ _putchar('\n');
+ return (0);
+}
