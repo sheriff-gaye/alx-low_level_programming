1b9ff20ff60fcf432e9cbf124f25dbaedc15279d
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-09 18:18:28 +0000

    c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,7 +6,7 @@
  *@n: The integer for the new node to contain.
  *
  *Return: If the function fails - NULL.
- *        Otherwise - the address of the new node.
+ *       Otherwise - the address of the new node.
  */
 dlistint_t* add_dnodeint(dlistint_t **head, const int n)
 {
