ab1e0c7347ab3d30188df5f65f6d14c3be7f2b0d
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-09 12:17:51 +0000

    C

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,19 +6,17 @@
  *
  * Return: number of nodes in the list
  */
-size_t print_dlistint(const dlistint_t *h)
-{
-	size_t nodes = 0;
+size_t print_dlistint(const dlistint_t *h) {
+  size_t nodes = 0;
 
-	if (!h)
-		return (0);
+  if (!h)
+    return (0);
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		nodes++;
-	}
+  while (h) {
+    printf("%d\n", h->n);
+    h = h->next;
+    nodes++;
+  }
 
-	return (nodes);
+  return (nodes);
 }
