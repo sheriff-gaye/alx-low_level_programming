c82bd553e836a69b57936d7874dd7b336f635e88
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-09 16:39:07 +0000

    c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,23 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of nodes in a doubly linked list
- * @h: pointer to the list
+ *dlistint_len - returns the number of nodes in a doubly linked list
+ *@h: pointer to the list
  *
- * Return: number of nodes
+ *Return: number of nodes
  */
-size_t dlistint_len(const dlistint_t *h) {
-  size_t nodes = 0;
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
 
-  if (!h)
-    return (0);
+	if (!h)
+		return (0);
 
-  while (h) {
-    nodes++;
-    h = h->next;
-  }
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
 
-  return (nodes);
+	return (nodes);
 }
--- 0x17-doubly_linked_lists/1-main.c
+++ 0x17-doubly_linked_lists/1-main.c
@@ -1,20 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * dlistint_len - Counts the number of elements in a linked dlistint_t list.
- * @h: The head of the dlistint_t list.
+ *main - check the code
  *
- * Return: The number of elements in the dlistint_t list.
+ *Return: Always EXIT_SUCCESS.
  */
-size_t dlistint_len(const dlistint_t *h)
+int main(void)
 {
-	size_t nodes = 0;
+	dlistint_t * head;
+	dlistint_t * new;
+	dlistint_t hello = { 8, NULL, NULL
+	};
+	size_t n;
 
-	while (h)
+	head = &hello;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 	{
-		nodes++;
-		h = h->next;
+		dprintf(2, "Error: Can't malloc\n");
+		return (EXIT_FAILURE);
 	}
-
-	return (nodes);
+	new->n = 9;
+	head->prev = new;
+	new->next = head;
+	new->prev = NULL;
+	head = new;
+	n = dlistint_len(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (EXIT_SUCCESS);
 }
