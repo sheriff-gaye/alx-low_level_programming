4f8fb764221a1a08eb02f193ace29f82839ec10a
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-09 21:00:07 +0000

    c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  *add_dnodeint_end - adds a new node at the end of a doubly linked list
  *@head: double pointer to the list
@@ -6,23 +7,26 @@
  *
  *Return: the address of the new element, or NULL if it failed
  */
-dlistint_t* add_dnodeint_end(dlistint_t **head, const int n) {
-	dlistint_t * new;
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
 	dlistint_t *temp = *head;
-	if (!head)
-			return (NULL);
+
+	if (!head)	
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
-			return (NULL);
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if (*head == NULL) {
+	if (*head == NULL)
+	{
 		new->prev = NULL;
 		*head = new;
 		return (new);
 	}
 	while (temp->next)
-			temp = temp->next;
+		temp = temp->next;
 	temp->next = new;
 	new->prev = temp;
 	return (new);
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,7 +7,7 @@
  *
  *Return: address of the node, or if it does not exist, NULL
  */
-dlistint_t* get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
